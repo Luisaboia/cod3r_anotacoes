6036b9ad61a11a3c4db2ee3e512f56dc42b5f538
Luis <ovokiller13@gmail.com>
2022-03-03 10:28:07 -0300

    exercicio 20 finalizado

--- Exercícios/Fundamentos/exercicio20.js
+++ Exercícios/Fundamentos/exercicio20.js
@@ -1,15 +1,29 @@
-function caixa(valor) {
-  const notas = [100, 50, 20, 10, 5, 1]
-  const troco = { 100: 0, 50: 0, 10: 0, 5: 0, 1: 0 }
+function caixa(compra, pago) {
+  const valorCompra = parseFloat(compra)
+  const valorPago = parseFloat(pago)
+  let troco = 0
 
-  for (let i of notas) {
-    while (valor >= i) {
-      troco[i] += 1
-      valor -= i
-    }
+  if (valorPago === valorCompra) {
+    console.log(`Não gerou troco.`)
+  } else if (valorPago > valorCompra) {
+    troco = valorPago - valorCompra
+    console.log(`${calcularNota(troco)}`)
+  } else {
+    console.log(`Não gerou troco por um erro inesperado.`)
   }
-  return troco
 }
 
-console.log(`Troco para 18: ${caixa(18)}`)
-console.log(`Troco para 200: ${caixa(200)}`)
+function calcularNota(n) {
+  const notas = [100, 50, 20, 10, 5, 1]
+  let texto = ''
+  for (let i = 0; i < notas.length; i++) {
+    if (n >= notas[i]) {
+      const calculo = Math.floor(n / notas[i])
+      texto += calculo + ` notas de ${notas[i]}\n`
+      n -= calculo * notas[i]
+    }
+  }
+  return texto
+}
+
+caixa(100, 273)
