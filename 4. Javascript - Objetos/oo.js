babdd134590dc201aa6b1c9c92dc830e1c18637c
Luis Saboia <luisaboia@hotmail.com>
2022-03-12 11:16:34 -0300

    Adicionado sessão de Orientação a Objetos

---
+++ 4. Javascript - Objetos/oo.js
@@ -0,0 +1,22 @@
+// Isto é um código não executável!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// Orientado a Objetos
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento() {
+    //...
+  }
+}
+objeto.processamento() // Foco passou a ser o objeto
+
+// Princípios importantes
+
+// 1. Abstração;
+// 2. Encapsulamento (OBS: Acoplamento grande não é uma boa prática);
+// 3. Herança (OBS: Priorizar a composição à herança);
+// 4. Polimorfismo
